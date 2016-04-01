63638417c635a4de08ba81fd741650e80d972f95
Wilfred Hughes <me@wilfred.me.uk>
2016-04-02 00:53:33 +0100

    Declare version.

--- cask-mode.el
+++ cask-mode.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2016
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Version: 0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
